9f33a5659f774062cbaedfa59d020fc5328c83b8
Carlh <carlhenryfortunatinfo@gmail.com>
2025-02-08 13:54:53 -0500

    Update services/api.js with api fix

--- services/api.js
+++ services/api.js
@@ -1,5 +1,5 @@
 export const sendBooking = async (data) => {
-    return await fetch("http://localhost:5000/send-email", {
+    return await fetch("https://mobile-barbershop-backend.onrender.com/send-email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
@@ -7,7 +7,7 @@ export const sendBooking = async (data) => {
   };
   
   export const sendContactMessage = async (data) => {
-    return await fetch("http://localhost:5000/api/contact", {
+    return await fetch("https://mobile-barbershop-backend.onrender.com/api/contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
