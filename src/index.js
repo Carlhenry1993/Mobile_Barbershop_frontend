f2c833e98cff84c8557f011bea72be6b4e9e3f33
Carlh <carlhenryfortunatinfo@gmail.com>
2025-05-26 22:00:22 -0400

    Update index.js for my repository

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Import React 18's createRoot
 import "./index.css"; // Your CSS
-import App from "./App.js"; // Your main App component
+import App from "./App"; // Your main App component
 
 // Get the root element from HTML and render the app
 const root = ReactDOM.createRoot(document.getElementById("root"));
